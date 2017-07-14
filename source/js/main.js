7c77d74382703879b6508466fd3dc264aa1ec38b
Deivid Marques <deividmarques@gmail.com>
2017-07-14 01:02:07 -0300

    add links fromt lectures

--- source/js/main.js
+++ source/js/main.js
@@ -75,109 +75,74 @@ var speakersData = [
 
 var timeline = [
   {
-    hour: '08:00 - 09:20',
-    title: 'Credenciamento',
-    description: 'Não esqueça seu RG',
-    speaker: '',
-    link: ''
-  },
-  {
-    hour: '9:20 - 9:30',
-    title: 'Apresentação',
-    description: '',
-    speaker: 'Matheus Marsiglio',
-    link: ''
-  },
-  {
     hour: '9:30 - 10:10',
     title: 'CSS Tips',
     description: 'Muitos de nós já perdemos a paciência com CSS pelo menos uma vez na vida. No entanto, já também demos aquele suspiro de alívio quando conseguimos resolver um determinado problema ou bug. Fácil de aprender e difícil de dominar, o CSS é uma linguagem de fácil acesso e que esconde diversos segredos. Nessa talk, iremos abordar algumas dicas de alguém que já perdeu a paciência algumas vezes com CSS mas que ainda o ama. Sem ressentimentos.',
     speaker: 'Raphael Fabeni',
-    link: ''
+    link: 'https://speakerdeck.com/raphaelfabeni/css-tips'
   },
   {
     hour: '10:20 - 11:00',
     title: 'Webapps confiáveis com Elm',
     description: 'Interfaces de usuário podem ser simples e confiáveis utilizando a ferramenta correta. Elm ajuda a impedir erros de execução e torna o processo de desenvolvimento mais simples. A simplicidade do Elm o ajuda a criar webapps à prova de balas e torna seu processo de desenvolvimento muito mais divertido.',
     speaker: 'Hugo Bessa',
-    link: ''
+    link: 'https://www.slideshare.net/hugobessaa/webapps-confiaveis-com-elm‬'
   },
   {
     hour: '11:10 - 12:00',
     title: ' UX: quem são, o que fazem, onde vivem?',
     description: '"Fazer UX" está virando pré-requisito para desenvolver qualquer produto digital, mas o que está por trás disso? O que o mercado espera que a gente faça, como os chefes vendem o que a gente faz, o que os devs acham que a gente faz e o que a gente realmente faz (ou deveria fazer).',
     speaker: 'Bianca Brancaleone',
-    link: ''
+    link: '/midia/palestra-ux.pdf'
   },
   {
     hour: '12:05 - 12:30',
     title: 'WCAG 2.1',
     description: 'Nesta palestra serão apresentadas as primeiras atualizações da nova versão das Diretrizes de Acessibilidade para Conteúdo Web, as WCAG, e como elas podem impactar a acessibilidade nas páginas Web. Serão mostradas as principais mudanças propostas nesse primeiro Draft e como isso pode refletir no desenvolvimento front-end.',
     speaker: 'Reinaldo Ferraz',
-    link: ''
-  },
-  {
-    hour: '12:30 - 14:00',
-    title: 'Almoço',
-    description: '',
-    speaker: '',
-    link: ''
+    link: '/midia/wcag21.pdf'
   },
   {
     hour: '14:10 - 14:50',
     title: 'Síndrome do impostor, um mal que nunca sai de moda',
     description: 'Você já teve a sensação que não é bom o suficiente para fazer o seu trabalho? Você já fingiu entender algo, mesmo sem entender nada? Você já teve medo de alguém descobrir que você é uma fraude? Que o código ou trabalho dos seus amigos é mais rápido ou melhor? Que você programa orientado ao Google? Então, talvez seja a hora de você entender melhor do que isso se trata e como vencer esse sentimento.',
     speaker: 'Patricia Silva',
-    link: ''
+    link: '/midia/sindrome-do-impostor.pdf'
   },
   {
     hour: '15:00 - 15:40',
     title: 'Renderizando componentes React no servidor',
     description: 'O ReactJS tem a poderosa habilidade de renderizar seus componentes no servidor. Nesta apresentação Bruno irá mostrar os benefícios e também como e quando podemos explorar esta prática.',
     speaker: 'Bruno Genaro',
-    link: ''
+    link: 'https://speakerdeck.com/brunogenaro/renderizando-componentes-react-no-servidor'
   },
   {
     hour: '15:50 - 16:10',
     title: 'Built for destruction: Preparando sua arquitetura de componentes para o refactor',
     description: 'Lições aprendidas ao criar um sistema de design auto documentável e de fácil refactor utilizando Functional CSS + React.',
     speaker: 'Zéh Fernandes',
-    link: ''
+    link: '/midia/build-for-destruction.pdf'
   },
   {
     hour: '16:20 - 16:50',
     title: 'What I learned interviewing for Front-end Developer roles ',
     description: 'Linguagem JavaScript. Desafios de código. Complexidade de algoritmos. CSS. Web APIs. React. E mais. Os aprendizados obtidos nos processos de entrevista para grandes nomes da tecnologia. Tudo isto contado de forma divertida (deixando de lado algumas frustrações). ',
     speaker: 'Jean Carlo Emer',
-    link: ''
-  },
-  {
-    hour: '17:00 - 17:30',
-    title: 'Coffee Break',
-    description: '',
-    speaker: '',
-    link: ''
+    link: 'https://www.slideshare.net/jeancarloemer/what-i-learned-interviewing-for-frontend-developer-roles'
   },
   {
     hour: '17:30 - 18:20',
     title: 'Vue.js, Vuex, Single Source of Truth e aplicações reais',
     description: 'O segredo para Single Page Applications bem sucedidas: o controle total sobre a informação carregada em memória. Esta palestra falará sobre gerenciamento centralizado do estado da aplicação.',
     speaker: 'Fábio Vedoveli',
-    link: ''
+    link: '/midia/vuejs.pdf'
   },
   {
     hour: '18:30 - 19:00',
-    title: 'Aguardando',
+    title: 'Por que ninguém se importa com seu novo projeto open source?',
     description: '',
     speaker: 'Zeno Rocha',
-    link: ''
-  },
-  {
-    hour: '19:00',
-    title: 'Party - Code In The Dark',
-    description: '',
-    speaker: '',
-    link: ''
+    link: 'https://speakerdeck.com/zenorocha/por-que-ninguem-se-importa-com-seu-novo-projeto-open-source'
   }
 ]
 
@@ -191,6 +156,7 @@ const timelineTemplate = (timeline) => {
             <p>${ item.hour }</p>
             <p>${ item.description }</p>
             <p class="timeline-speaker">${ item.speaker }</p>
+            <a href="${ item.link }" target="_blank">Slide da palestra</a>
           </div>
         </div>
       `
